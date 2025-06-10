d65cd6ed9e6b62145dffd36c82c16355dabf4a8f
villarta-johnkeith <johnkeithvillarta3@gmail.com>
2025-06-11 00:07:44 +0800

    Add class skeleton for profile

---
+++ polytech/villarta.py
@@ -0,0 +1,43 @@
+from colorama import Fore
+import os
+
+UNSET_OPTION = '-1'
+EXIT_OPTION = '0'
+
+class Profile:
+    def __init__(self, name, age, birthday, hobbies, nationality):
+        self.name = name
+        self.age = age
+        self.birthday = birthday
+        self.hobbies = hobbies
+        self.nationality = nationality
+
+    def display_name(self):
+        pass
+
+    def display_age(self):
+        pass
+
+    def display_birthday(self):
+        pass
+
+    def display_hobbies(self):
+        pass
+
+    def display_nationality(self):
+        pass
+
+    def display_summary(self):
+        pass
+
+    def input_profile(self):
+        pass
+
+    def show_menu(self):
+        pass
+
+    def handle_choice(self, choice):
+        pass
+
+    def menu():
+        pass
