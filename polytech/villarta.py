b544f9ec210ea6a1346252781a7c2e0138e33d5b
villarta-johnkeith <johnkeithvillarta3@gmail.com>
2025-06-11 00:18:22 +0800

    Add display summary

--- polytech/villarta.py
+++ polytech/villarta.py
@@ -43,7 +43,21 @@ class Profile:
         print(f"\nNationality: {Fore.CYAN}{self.nationality}{Fore.RESET}")
 
     def display_summary(self):
-        pass
+        def display_summary(self):
+        print(Fore.YELLOW + "=" * 40)
+        print("        ✨ PERSONAL PROFILE ✨")
+        print("=" * 40 + Fore.RESET)
+
+        if (not self.name or not self.age or not self.birthday or 
+            not self.hobbies or not self.nationality):
+            print(" Please complete your profile details first.")
+            return
+        print(f"{Fore.CYAN}Name        :{Fore.RESET} {self.name}")
+        print(f"{Fore.CYAN}Age         :{Fore.RESET} {self.age}")
+        print(f"{Fore.CYAN}Birthday    :{Fore.RESET} {self.birthday}")
+        print(f"{Fore.CYAN}Hobbies     :{Fore.RESET} {self.hobbies}")
+        print(f"{Fore.CYAN}Nationality :{Fore.RESET} {self.nationality}")
+        print(Fore.YELLOW + "=" * 50 + Fore.RESET)
 
     def input_profile(self):
         pass
