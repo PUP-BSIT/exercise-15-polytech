10f4f592f75b93a9eb8c7e1ef1ceb863a6160670
victorio-kalelle <kalelleeee@gmail.com>
2025-06-11 00:18:41 +0800

    import and integrate colorama for better visuals

--- polytech/victorio.py
+++ polytech/victorio.py
@@ -1,20 +1,24 @@
 import os
+from colorama import init, Fore
+
 EXIT_OPTION = 0
+
+init(autoreset = True)
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def display_menu():
-    print("=" * 55)
-    print("\t   ⟡˙⋆ Victorio's Diary System ⋆˙⟡ ")
-    print("=" * 55)
-    print("[1.] Add Mood")
-    print("[2.] Write Journal Entry")
-    print("[3.] View Journal Entries")
-    print("[4.] Delete an Entry")
-    print("[5.] Show Summary")
-    print("[0.] Back to Main Menu")
-    print("=" * 55)
+    print(Fore.MAGENTA + "=" * 55)
+    print( "   ⟡˙⋆ Victorio's Diary System ⋆˙⟡ ")
+    print(Fore.MAGENTA + "=" * 55)
+    print("[1.]" + Fore.MAGENTA + " Add Mood")
+    print("[2.]" + Fore.MAGENTA + " Write Journal Entry")
+    print("[3.]" + Fore.MAGENTA + " View Journal Entries")
+    print("[4.]" + Fore.MAGENTA + " Delete an Entry")
+    print("[5.]" + Fore.MAGENTA + " Show Summary")
+    print("[0.]" + Fore.MAGENTA + " Back to Main Menu")
+    print(Fore.MAGENTA + "=" * 55)
 
 def process_choice(choice):
     clear_screen()
@@ -30,14 +34,14 @@ def process_choice(choice):
         case 5:
             pass
         case _:
-            print("Invalid choice. Try again.")
+            print(Fore.RED + "Invalid choice. Try again.")
     input("\nPress Enter to Continue...")
 
 def get_user_choice():
     try:
-        return int(input("Enter your choice: "))
+        return int(input(Fore.MAGENTA + "Enter your choice: "))
     except ValueError:
-        print("Invalid input. Please enter a number.")
+        print(Fore.RED + "Invalid input. Please enter a number.")
         return None
 
 def kalelle():
@@ -50,7 +54,7 @@ def kalelle():
             continue
 
         if choice == EXIT_OPTION:
-            print("Going back to Main Menu.")
+            print(Fore.CYAN + "Going back to Main Menu.")
             break
 
         process_choice(choice)
